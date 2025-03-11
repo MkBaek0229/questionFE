7a7334557aefd881e6acbaa5dc75806273308869
Minki Baek <alsrl6678@naver.com>
2025-03-11 15:25:01 +0900

    nginx test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import { useRecoilState } from "recoil";
 import { useEffect } from "react";
-import axios from "../axiosConfig";
+import axios from "../src/axiosConfig"; // ✅ axiosConfig 불러오기
 import {
   authState,
   expertAuthState,
